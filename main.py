354cd3ccf7a03c6c3a57152a232f9ec012ca7545
JoPanta <jopantaleao@gmail.com>
2023-12-02 22:08:35 +0000

    done

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
+# this one gets all King's books and their publishing date
+
 # driver.get("https://stephenking.com/works/novel/index.html")
 #
 # books = driver.find_elements(By.CLASS_NAME, value="works-title")
@@ -23,6 +25,7 @@ driver = webdriver.Chrome(options=chrome_options)
 #
 # driver.close()
 
+#this one gets 100 most played games on steam and their numbers
 driver.get("https://steamdb.info/charts/")
 
 table = driver.find_element(By.CLASS_NAME, 'table-products')
